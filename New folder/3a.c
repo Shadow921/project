007f26b2b81e3bd639c72659c1373aaf600afc6e
Shadow921 <89244607+Shadow921@users.noreply.github.com>
2025-06-10 10:55:07 +0530

    3a.c

---
+++ New folder/3a.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define INF 999
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+void floyd(int p[][10], int n) {
+    int i, j, k;
+    for (k = 0; k < n; k++)
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                p[i][j] = min(p[i][j], p[i][k] + p[k][j]);
+}
+
+int main() {
+    int a[10][10], n, i, j;
+
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix (use %d for INF):\n", INF);
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    floyd(a, n);
+
+    printf("\nShortest path matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%3d ", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
