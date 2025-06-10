374a1ff64c3ff14bddc1144e03de862eed0cc90b
Shadow921 <89244607+Shadow921@users.noreply.github.com>
2025-06-10 12:30:23 +0530

    3a.c

--- New folder/3a.c
+++ New folder/3a.c
@@ -35,3 +35,39 @@ int main() {
 
     return 0;
 }
+
+//3b
+#include <stdio.h>
+
+void warshall(int p[][10], int n)
+{
+    int i, j, k;
+    for (k = 0; k < n; k++)
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                p[i][j] = p[i][j] || (p[i][k] && p[k][j]);
+}
+
+int main()
+{
+    int a[10][10], n, i, j;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    warshall(a, n);
+
+    printf("Transitive closure (Path matrix):\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
